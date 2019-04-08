8d8b0b9906cb8ff9c9e613710a8c4e08031fd841
Tyler Hunt <tyhunt04@gmail.com>
2019-04-08 21:57:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class App < Sinatra::Base
   get '/multiply/:num1/:num2' do
     first = params[:num1]
     last = params[:num2]
-    @product = (@first * @last)
+    @product = @first * @last
     "#{@product}"
   end
 
